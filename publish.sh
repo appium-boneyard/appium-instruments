24ddfa4bc047fd09990ad15162b9fc3eb3149f15
sebv <seb.vincent@gmail.com>
2014-03-15 10:50:29 +0800

    hacking

---
+++ publish.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+if [ -n "$1" ]; then
+    UPGRADE_TYPE=$1
+else
+    UPGRADE_TYPE=patch
+fi
+if [ -n "$(git status -s)" ]; then
+    echo 'Error: Uncommited stuff.'
+    exit 1
+fi
+set -x
+git pull origin master
+TAG=$(npm version $UPGRADE_TYPE)
+echo "Upgrading to version $TAG"
+git commit -a -m $TAG
+git push --tags origin master
+git push -f origin master:published
+#npm publish
