682c65ff399de854e8af61eff7d9938a7958430c
Russ Van Bert <russell.van.bert@myob.com>
2016-08-09 10:26:47 +1000

    Remove <Type> from PlistBuddy Set commands.
    `/usr/libexec/PlistBuddy -h` suggests <Type> should be excluded when
    calling the `Set` command.
    
    Set <Entry> <Value> - Sets the value at Entry to Value
    Add <Entry> <Type> [<Value>] - Adds Entry to the plist, with value Value

--- bin/xcode-iwd.sh
+++ bin/xcode-iwd.sh
@@ -13,12 +13,12 @@ if /usr/libexec/PlistBuddy -c "Add :EnvironmentVariables:DYLD_INSERT_LIBRARIES s
     echo "Added :EnvironmentVariables:DYLD_INSERT_LIBRARIES"
 else
     echo ":EnvironmentVariables:DYLD_INSERT_LIBRARIES already exists. Resetting."
-    /usr/libexec/PlistBuddy -c "Set :EnvironmentVariables:DYLD_INSERT_LIBRARIES string $iwd_path/DTMobileISShim.dylib" $plist_path
+    /usr/libexec/PlistBuddy -c "Set :EnvironmentVariables:DYLD_INSERT_LIBRARIES $iwd_path/DTMobileISShim.dylib" $plist_path
 fi
 
 if /usr/libexec/PlistBuddy -c "Add :EnvironmentVariables:LIB_PATH string $iwd_path/" $plist_path ; then
     echo "Added :EnvironmentVariables:LIB_PATH"
 else
     echo ":EnvironmentVariables:LIB_PATH already exists. Resetting."
-    /usr/libexec/PlistBuddy -c "Set :EnvironmentVariables:LIB_PATH string $iwd_path/" $plist_path
+    /usr/libexec/PlistBuddy -c "Set :EnvironmentVariables:LIB_PATH $iwd_path/" $plist_path
 fi
